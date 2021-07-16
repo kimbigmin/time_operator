2131037c53687f655b9b6f56d4f61ea6d90f6db0
kimmingyu <alsrb2918@gmail.com>
2021-07-17 01:46:49 +0900

    Amend indent

--- time_operator.js
+++ time_operator.js
@@ -1,36 +1,36 @@
 
-        function showTime() {
+function showTime() {
 
-            const beforeTimeHour = document.querySelector('#beforeTimeHour').value;
-            const afterTimeHour = document.querySelector('#afterTimeHour').value;
-            const beforeTimeMin = parseInt(document.querySelector('#beforeTimeMin').value);
-            const afterTimeMin = parseInt(document.querySelector('#afterTimeMin').value); 
-        
-            
-            let beforeSum =(beforeTimeHour*60)+beforeTimeMin;
-            let afterSum =(afterTimeHour*60)+afterTimeMin;
-            let difference = ((afterSum - beforeSum) / beforeSum)*100;
-            let differenceInt = Math.round(difference);
-        
-        
-            document.querySelector('#showResult').innerHTML = `이전 기준과 현재 기준의 시간 퍼센트 차이는 약 <span>${differenceInt}%</span> 입니다.`
+    const beforeTimeHour = document.querySelector('#beforeTimeHour').value;
+    const afterTimeHour = document.querySelector('#afterTimeHour').value;
+    const beforeTimeMin = parseInt(document.querySelector('#beforeTimeMin').value);
+    const afterTimeMin = parseInt(document.querySelector('#afterTimeMin').value); 
 
-            document.querySelector('#newReset').innerHTML =`<button onclick="resetBtn()" class="reset">초기화</button>`
-            
-        }
-        
-        function resetBtn() {
+    
+    let beforeSum =(beforeTimeHour*60)+beforeTimeMin;
+    let afterSum =(afterTimeHour*60)+afterTimeMin;
+    let difference = ((afterSum - beforeSum) / beforeSum)*100;
+    let differenceInt = Math.round(difference);
 
-            document.getElementById('beforeTimeHour').value = '';
-            document.getElementById('afterTimeHour').value = '';
-            document.getElementById('beforeTimeMin').value = '';
-            document.getElementById('afterTimeMin').value = '';
-            document.getElementById('beforeTimeHour').focus();
 
-            const result = document.getElementById('showResult');
-            const removeReset = document.getElementById('newReset');
+    document.querySelector('#showResult').innerHTML = `이전 기준과 현재 기준의 시간 퍼센트 차이는 약 <span>${differenceInt}%</span> 입니다.`
 
-            result.innerHTML = '';
-            removeReset.innerHTML = '';
+    document.querySelector('#newReset').innerHTML =`<button onclick="resetBtn()" class="reset">초기화</button>`
+    
+}
 
-        }
+function resetBtn() {
+
+    document.getElementById('beforeTimeHour').value = '';
+    document.getElementById('afterTimeHour').value = '';
+    document.getElementById('beforeTimeMin').value = '';
+    document.getElementById('afterTimeMin').value = '';
+    document.getElementById('beforeTimeHour').focus();
+
+    const result = document.getElementById('showResult');
+    const removeReset = document.getElementById('newReset');
+
+    result.innerHTML = '';
+    removeReset.innerHTML = '';
+
+}
