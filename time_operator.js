3af8e75d07f5dd8b5a945c3e7f05c75d9e1780ba
kimmingyu <alsrb2918@gmail.com>
2021-07-25 00:35:13 +0900

    Time operator v1.1.1 디자인 변경

--- time_operator.js
+++ time_operator.js
@@ -13,18 +13,27 @@ function showTime() {
     let differenceInt = Math.round(difference);
 
 
-    document.querySelector('#showResult').innerHTML = `<p class="result_text">이전 기준과 현재 기준의 시간 퍼센트 차이는 약 <span>${differenceInt}%</span> 입니다.</p>`
+    if (differenceInt > 0) {
+        
+        document.querySelector('#showResult').innerHTML = `<p class="result_text">이전 시간보다 약 <span style="color:red;">${differenceInt}%</span> <strong style="color:red;">증가</strong>하였습니다!</p>`
 
-    document.querySelector('#newReset').innerHTML =`<button onclick="resetBtn()" class="reset">Reset</button>`
-    
+        document.querySelector('#newReset').innerHTML =`<button onclick="percentResetBtn()" class="reset">Reset</button>`
+    } else {
+        document.querySelector('#showResult').innerHTML = `<p class="result_text">이전 시간보다 약 <span style="color:blue;">${differenceInt}%</span> <strong style="color:blue;">감소</strong>하였습니다.</p>`
+
+        document.querySelector('#newReset').innerHTML =`<button onclick="percentResetBtn()" class="reset">Reset</button>`
+    }
 }
 
-function resetBtn() {
 
+
+function percentResetBtn() {
+    
     document.getElementById('beforeTimeHour').value = '';
     document.getElementById('afterTimeHour').value = '';
     document.getElementById('beforeTimeMin').value = '';
     document.getElementById('afterTimeMin').value = '';
+    
     document.getElementById('beforeTimeHour').focus();
 
     const result = document.getElementById('showResult');
@@ -32,5 +41,118 @@ function resetBtn() {
 
     result.innerHTML = '';
     removeReset.innerHTML = '';
+}
 
+
+
+const daysHour = ['#monHour', '#tueHour', '#wenHour', '#thuHour', '#friHour', '#satHour', '#sunHour'];
+
+const daysMin = ['#monMin', '#tueMin', '#wenMin', '#thuMin', '#friMin', '#satMin', '#sunMin'];
+
+
+
+function avgResetBtn() {
+
+    for(i = 0; i < daysHour.length; i++) {
+        document.querySelector(daysHour[i]).value = null;
+    }
+
+    for(i = 0; i < daysMin.length; i++) {
+        document.querySelector(daysMin[i]).value = null;
+    }
+
+    // const result = document.getElementById('showResult');
+    // const removeReset = document.getElementById('newReset');
+
+    // result.innerHTML = '';
+    // removeReset.innerHTML = '';
+
+    document.getElementById('avgText').style.visibility = "hidden";
+    document.getElementById('avgBtn').style.display= "none";
+}   
+
+
+
+// ------------ 평균 시간 구하기 part ------------ //
+
+function getAverageTime() {
+
+    const daysHour = ['#monHour', '#tueHour', '#wenHour', '#thuHour', '#friHour', '#satHour', '#sunHour'];
+
+    const daysMin = ['#monMin', '#tueMin', '#wenMin', '#thuMin', '#friMin', '#satMin', '#sunMin'];
+
+
+    // html input box 시간 값 가져와서 합산
+    let sumHours = 0;
+    let sumMins = 0;
+
+    for(i = 0; i < daysHour.length; i++) {
+        let numHour = parseInt(document.querySelector(daysHour[i]).value);
+        
+        sumHours += numHour;
+    }
+
+    for(i = 0; i < daysMin.length; i++) {
+        let numMin = parseInt(document.querySelector(daysMin[i]).value);
+        
+        sumMins += numMin;
+    }
+
+
+    // 구한 값 시간 계산하가
+    const minsToHour = parseInt(sumMins / 60);
+
+    let hourToMin = parseInt((sumHours + minsToHour) * 60);
+    let minToMin = (sumMins % 60);
+
+    let ResultToOper = ((hourToMin + minToMin) / 60) / 7;
+    const resultHour = parseInt(ResultToOper);
+    const resultMin = Math.floor(((ResultToOper - resultHour)* 0.6) * 100);
+    
+
+    document.querySelector('#showResult').innerHTML = `<p class="result_text_avg" id="avgText">평균 시간은 <strong>${resultHour}시간 ${resultMin}분</strong> 입니다.</p>`
+
+    document.querySelector('#newReset').innerHTML =`<button onclick="avgResetBtn()" class="reset" id="avgBtn">Reset</button>`
+    
+}
+
+
+
+
+// 시간 더하기 
+function getSumTime() {
+
+    const daysHour = ['#monHour', '#tueHour', '#wenHour', '#thuHour', '#friHour', '#satHour', '#sunHour'];
+
+    const daysMin = ['#monMin', '#tueMin', '#wenMin', '#thuMin', '#friMin', '#satMin', '#sunMin'];
+
+
+    // html input box 시간 값 가져와서 합산
+    let sumHours = 0;
+    let sumMins = 0;
+
+    for(i = 0; i < daysHour.length; i++) {
+        let numHour = parseInt(document.querySelector(daysHour[i]).value);
+        
+        sumHours += numHour;
+    }
+
+    for(i = 0; i < daysMin.length; i++) {
+        let numMin = parseInt(document.querySelector(daysMin[i]).value);
+        
+        sumMins += numMin;
+    }
+
+
+    // 구한 값 시간 계산하가
+    const minsToHour = parseInt(sumMins / 60);
+
+    let resultHour = parseInt(sumHours + minsToHour);
+    let resultMin = (sumMins % 60);
+    
+
+    document.querySelector('#showResult').innerHTML = `<p class="result_text_avg" id="avgText">총합 시간은 <strong>${resultHour}시간 ${resultMin}분</strong> 입니다.</p>`
+
+    document.querySelector('#newReset').innerHTML =`<button onclick="avgResetBtn()" class="reset" id="avgBtn">Reset</button>`
+    
 }
